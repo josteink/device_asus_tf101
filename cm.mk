6c94020d7df74ef48e90199577eba3a17adb5039
TheMrcool212 <munirmohamadrani@gmail.com>
2013-06-06 12:32:42 +0800

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -10,7 +10,7 @@ $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 PRODUCT_NAME := cm_tf101
 PRODUCT_BRAND := asus
 PRODUCT_DEVICE := tf101
-PRODUCT_MODEL := Transformer
+PRODUCT_MODEL := Transformer Pad
 PRODUCT_MANUFACTURER := asus
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=US_epad BUILD_FINGERPRINT=asus/US_epad/EeePad:4.0.3/IML74K/US_epad-9.2.1.11-20120221:user/release-keys PRIVATE_BUILD_DESC="US_epad-user 4.0.3 IML74K US_epad-9.2.1.11-20120221 release-keys"
 
